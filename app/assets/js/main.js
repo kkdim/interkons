5f30c6ad55f4ca2d172bb52797ee2be785dbf5c9
dimitrije <kovacevic.d14@gmail.com>
2018-06-20 00:18:32 +0200

    update 1.2

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,26 +1,12 @@
-/////////Slider////////
+
 $(document).ready(function(){
-	$('.next').on('click',function(){
-		var currentImg = $('.active');
-		var nextImg = currentImg.next();
-
-		if(nextImg.length){
-			currentImg.removeClass('active').css('z-index', -10);
-			nextImg.addClass('active').css('z-index', 10);
-		}
-	});
-
-	$('.prev').on('click',function(){
-		var currentImg = $('.active');
-		var prevImg = currentImg.prev();
-
-		if(prevImg.length){
-			currentImg.removeClass('active').css('z-index', -10);
-			prevImg.addClass('active').css('z-index', 10);
-		}
-	});
-
-//////Scroll////////
+	//slider
+$('.sl').slick({
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+	//scroll
 	var documentElem = $(document),
 		nav = $('nav'),
 		lastScrollTop = 0;
@@ -32,15 +18,4 @@ $(document).ready(function(){
 			else nav.removeClass('hidden');
 			lastScrollTop = currentScrollTop;
 		});
-
-
-//////Hover text animation///
-
-
-    $('.item-animation-1').hover(
-         function(){ $(this).addClass('.animated pulse') },
-         function(){ $(this).removeClass('.animated pulse') }
-    )
-
 });
-
